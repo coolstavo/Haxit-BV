b830460ec8057b9856923591d84749edbe1675cf
mart prenger <mart.prenger@gmail.com>
2026-01-07 15:05:09 +0100

    basis zoek functionaliteit

--- src/main/java/com/webapp/hexit/HomeController.java
+++ src/main/java/com/webapp/hexit/HomeController.java
@@ -3,6 +3,7 @@ package com.webapp.hexit;
 import com.webapp.hexit.model.Event;
 import com.webapp.hexit.repository.EventRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +18,31 @@ public class HomeController {
   }
 
   @GetMapping("/")
-  public String home(Model model) {
+  public String home(
+    @RequestParam(name = "titel", required = false) String titel,
+    @RequestParam(name = "type", required = false) String type,
+    Model model
+  ) {
     List<Event> events = eventRepository.findAll();
+
+    if (titel != null && !titel.isBlank()) {
+      events = events
+        .stream()
+        .filter(event ->
+          event.getTitle().toLowerCase().contains(titel.toLowerCase())
+        )
+        .collect(Collectors.toList());
+    }
+
+    if (type != null && !type.isBlank()) {
+      events = events
+        .stream()
+        .filter(event ->
+          event.getType().toLowerCase().contains(type.toLowerCase())
+        )
+        .collect(Collectors.toList());
+    }
+
     model.addAttribute("events", events);
     model.addAttribute("username", "Gast");
     model.addAttribute("loginRequired", false);
