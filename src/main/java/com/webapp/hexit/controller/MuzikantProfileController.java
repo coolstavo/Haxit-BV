4a05da5ee279facf41026ef71f64351e8d85cc1a
shanos123 <60260010+shanos123@users.noreply.github.com>
2026-01-21 19:49:14 +0100

    changes

--- src/main/java/com/webapp/hexit/controller/MuzikantProfileController.java
+++ src/main/java/com/webapp/hexit/controller/MuzikantProfileController.java
@@ -21,6 +21,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,206 +34,217 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class MuzikantProfileController {
 
-  private final UserRepository userRepository;
-  private final MuzikantRepository muzikantRepository;
-  private final MuzikantInstrumentRepository muzikantInstrumentRepository;
-  private final InstrumentRepository instrumentRepository;
-  private final GenreRepository genreRepository;
-  private final ProfileFileRepository profileFileRepository;
-  private final JamRepository jamRepository;
+    private final UserRepository userRepository;
+    private final MuzikantRepository muzikantRepository;
+    private final MuzikantInstrumentRepository muzikantInstrumentRepository;
+    private final InstrumentRepository instrumentRepository;
+    private final GenreRepository genreRepository;
+    private final ProfileFileRepository profileFileRepository;
+    private final JamRepository jamRepository;
 
-  public MuzikantProfileController(
-    UserRepository userRepository,
-    MuzikantRepository muzikantRepository,
-    MuzikantInstrumentRepository muzikantInstrumentRepository,
-    InstrumentRepository instrumentRepository,
-    GenreRepository genreRepository,
-    ProfileFileRepository profileFileRepository,
-    JamRepository jamRepository
-  ) {
-    this.userRepository = userRepository;
-    this.muzikantRepository = muzikantRepository;
-    this.muzikantInstrumentRepository = muzikantInstrumentRepository;
-    this.instrumentRepository = instrumentRepository;
-    this.genreRepository = genreRepository;
-    this.profileFileRepository = profileFileRepository;
-    this.jamRepository = jamRepository;
-  }
-
-  public String getProfile(String username, Model model) {
-    User profile = userRepository.findByUsername(username).orElse(null);
-
-    if (profile == null) return handleError(model);
-
-    Muzikant muzikant = muzikantRepository.findByUser(profile).orElse(null);
-    if (muzikant != null) {
-      // CONVERT BLOB TO BASE64
-      if (muzikant.getProfilePic() != null) {
-        String base64Image = Base64.getEncoder().encodeToString(
-          muzikant.getProfilePic()
-        );
-        model.addAttribute("muzikantImage", base64Image);
-      }
-      List<MuzikantInstrument> muzikantInstruments =
-        muzikantInstrumentRepository.findByMuzikantId(muzikant.getId());
-      List<Instrument> allInstruments = instrumentRepository.findAll();
-
-      model.addAttribute("muzikant", muzikant);
-      model.addAttribute("muzikantInstruments", muzikantInstruments);
-      model.addAttribute("allInstruments", allInstruments);
-      List<Genre> allGenres = genreRepository.findAll();
-      model.addAttribute("allGenres", allGenres);
-
-      // Content ophalen van gebruiker
-      List<Profile_File> userContent =
-        profileFileRepository.findByUser_UsernameOrderByUploadDateDesc(
-          username
-        );
-      model.addAttribute("mediaBestanden", userContent);
-
-      // Get jams created by this muzikant
-      List<Jam> jams = jamRepository.findByMuzikantUser(profile);
-      model.addAttribute("jams", jams);
-
-      return "profile-muzikant";
-    }
-    return handleError(model);
-  }
-
-  public String getProfileEdit(String username, Model model) {
-    User profile = userRepository
-      .findByUsername(username)
-      .orElseThrow(() -> new RuntimeException("Gebruiker niet gevonden"));
-
-    Muzikant muzikant = muzikantRepository
-      .findByUser(profile)
-      .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
-
-    List<MuzikantInstrument> muzikantInstruments =
-      muzikantInstrumentRepository.findByMuzikantId(muzikant.getId());
-    List<Instrument> allInstruments = instrumentRepository.findAll();
-
-    model.addAttribute("muzikant", muzikant);
-    model.addAttribute("muzikantInstruments", muzikantInstruments);
-    model.addAttribute("allInstruments", allInstruments);
-    List<Genre> allGenres = genreRepository.findAll();
-    model.addAttribute("allGenres", allGenres);
-    return "profile-edit-muzikant";
-  }
-
-  @PostMapping("/profile/change-profile-pic")
-  public String saveMuzikantProfilePic(
-    @ModelAttribute Muzikant muzikant,
-    @RequestParam("imageFile") MultipartFile file
-  ) throws IOException {
-    Muzikant existingMuzikant = muzikantRepository
-      .findById(muzikant.getId())
-      .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
-
-    if (!file.isEmpty()) {
-      existingMuzikant.setProfilePic(file.getBytes());
+    public MuzikantProfileController(
+        UserRepository userRepository,
+        MuzikantRepository muzikantRepository,
+        MuzikantInstrumentRepository muzikantInstrumentRepository,
+        InstrumentRepository instrumentRepository,
+        GenreRepository genreRepository,
+        ProfileFileRepository profileFileRepository,
+        JamRepository jamRepository
+    ) {
+        this.userRepository = userRepository;
+        this.muzikantRepository = muzikantRepository;
+        this.muzikantInstrumentRepository = muzikantInstrumentRepository;
+        this.instrumentRepository = instrumentRepository;
+        this.genreRepository = genreRepository;
+        this.profileFileRepository = profileFileRepository;
+        this.jamRepository = jamRepository;
     }
 
-    muzikantRepository.save(existingMuzikant);
+    public String getProfile(String username, Model model) {
+        User profile = userRepository.findByUsername(username).orElse(null);
 
-    return "redirect:/profile/" + existingMuzikant.getUser().getUsername();
-  }
+        if (profile == null) return handleError(model);
 
-  @PostMapping("/profile/save-muzikant")
-  public String saveMuzikantProfile(@ModelAttribute Muzikant muzikant) {
-    Muzikant existingMuzikant = muzikantRepository
-      .findById(muzikant.getId())
-      .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
+        Muzikant muzikant = muzikantRepository.findByUser(profile).orElse(null);
+        if (muzikant != null) {
+            // CONVERT BLOB TO BASE64
+            if (muzikant.getProfilePic() != null) {
+                String base64Image = Base64.getEncoder().encodeToString(
+                    muzikant.getProfilePic()
+                );
+                model.addAttribute("muzikantImage", base64Image);
+            }
+            List<MuzikantInstrument> muzikantInstruments =
+                muzikantInstrumentRepository.findByMuzikantId(muzikant.getId());
+            List<Instrument> allInstruments = instrumentRepository.findAll();
 
-    existingMuzikant.setLeeftijd(muzikant.getLeeftijd());
+            model.addAttribute("muzikant", muzikant);
+            model.addAttribute("muzikantInstruments", muzikantInstruments);
+            model.addAttribute("allInstruments", allInstruments);
+            List<Genre> allGenres = genreRepository.findAll();
+            model.addAttribute("allGenres", allGenres);
 
-    List<Instrument> selectedInstruments = muzikant.getInstruments();
-    existingMuzikant.setInstruments(selectedInstruments);
+            // Content ophalen van gebruiker
+            List<Profile_File> userContent =
+                profileFileRepository.findByUser_UsernameOrderByUploadDateDesc(
+                    username
+                );
+            model.addAttribute("mediaBestanden", userContent);
 
-    List<Genre> selectedGenres = muzikant.getGenres();
-    existingMuzikant.setGenres(selectedGenres);
-    muzikantRepository.save(existingMuzikant);
-    return "redirect:/profile/" + existingMuzikant.getUser().getUsername();
-  }
+            // Get jams created by this muzikant
+            List<Jam> jams = jamRepository.findByMuzikantUser(profile);
+            model.addAttribute("jams", jams);
 
-  @PostMapping("/profile/add-instrument")
-  public String addInstrument(
-    @RequestParam Long muzikantId,
-    @RequestParam Long instrumentId,
-    @RequestParam String level
-  ) {
-    Muzikant muzikant = muzikantRepository
-      .findById(muzikantId)
-      .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
+            return "profile-muzikant";
+        }
+        return handleError(model);
+    }
 
-    Instrument instrument = instrumentRepository
-      .findById(instrumentId)
-      .orElseThrow(() -> new RuntimeException("Instrument niet gevonden"));
+    @GetMapping("/profile/edit/{username}")
+    public String getProfileEdit(@PathVariable String username, Model model) {
+        User profile = userRepository
+            .findByUsername(username)
+            .orElseThrow(() -> new RuntimeException("Gebruiker niet gevonden"));
 
-    MuzikantInstrument muzikantInstrument = new MuzikantInstrument();
-    muzikantInstrument.setMuzikant(muzikant);
-    muzikantInstrument.setInstrument(instrument);
-    muzikantInstrument.setLevel(level);
+        Muzikant muzikant = muzikantRepository
+            .findByUser(profile)
+            .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
 
-    muzikantInstrumentRepository.save(muzikantInstrument);
+        List<MuzikantInstrument> muzikantInstruments =
+            muzikantInstrumentRepository.findByMuzikantId(muzikant.getId());
+        List<Instrument> allInstruments = instrumentRepository.findAll();
 
-    return "redirect:/profile/" + muzikant.getUser().getUsername() + "/edit";
-  }
+        model.addAttribute("muzikant", muzikant);
+        model.addAttribute("muzikantInstruments", muzikantInstruments);
+        model.addAttribute("allInstruments", allInstruments);
+        List<Genre> allGenres = genreRepository.findAll();
+        model.addAttribute("allGenres", allGenres);
+        return "profile-edit-muzikant";
+    }
 
-  @PostMapping("/profile/remove-instrument")
-  public String removeInstrument(@RequestParam Long muzikantInstrumentId) {
-    Muzikant muzikant = muzikantInstrumentRepository
-      .findById(muzikantInstrumentId)
-      .orElseThrow(() ->
-        new RuntimeException("MuzikantInstrument niet gevonden")
-      )
-      .getMuzikant();
+    @PostMapping("/profile/change-profile-pic")
+    public String saveMuzikantProfilePic(
+        @ModelAttribute Muzikant muzikant,
+        @RequestParam("imageFile") MultipartFile file
+    ) throws IOException {
+        Muzikant existingMuzikant = muzikantRepository
+            .findById(muzikant.getId())
+            .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
 
-    muzikantInstrumentRepository.deleteById(muzikantInstrumentId);
-    return "redirect:/profile/" + muzikant.getUser().getUsername() + "/edit";
-  }
+        if (!file.isEmpty()) {
+            existingMuzikant.setProfilePic(file.getBytes());
+        }
 
-  @PostMapping("/profile/add-genre")
-  public String addGenre(
-    @RequestParam Long muzikantId,
-    @RequestParam Long genreId
-  ) {
-    Muzikant muzikant = muzikantRepository
-      .findById(muzikantId)
-      .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
+        muzikantRepository.save(existingMuzikant);
 
-    Genre genre = genreRepository
-      .findById(genreId)
-      .orElseThrow(() -> new RuntimeException("Genre niet gevonden"));
+        return "redirect:/profile/" + existingMuzikant.getUser().getUsername();
+    }
 
-    muzikant.getGenres().add(genre);
-    muzikantRepository.save(muzikant);
+    @PostMapping("/profile/save-muzikant")
+    public String saveMuzikantProfile(@ModelAttribute Muzikant muzikant) {
+        Muzikant existingMuzikant = muzikantRepository
+            .findById(muzikant.getId())
+            .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
 
-    return "redirect:/profile/" + muzikant.getUser().getUsername() + "/edit";
-  }
+        existingMuzikant.setLeeftijd(muzikant.getLeeftijd());
 
-  @PostMapping("/profile/remove-genre")
-  public String removeGenre(
-    @RequestParam Long muzikantId,
-    @RequestParam Long genreId
-  ) {
-    Muzikant muzikant = muzikantRepository
-      .findById(muzikantId)
-      .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
+        List<Instrument> selectedInstruments = muzikant.getInstruments();
+        existingMuzikant.setInstruments(selectedInstruments);
 
-    muzikant.getGenres().removeIf(genre -> genre.getId().equals(genreId));
-    muzikantRepository.save(muzikant);
+        List<Genre> selectedGenres = muzikant.getGenres();
+        existingMuzikant.setGenres(selectedGenres);
+        muzikantRepository.save(existingMuzikant);
+        return "redirect:/profile/" + existingMuzikant.getUser().getUsername();
+    }
 
-    return "redirect:/profile/" + muzikant.getUser().getUsername() + "/edit";
-  }
+    @PostMapping("/profile/add-instrument")
+    public String addInstrument(
+        @RequestParam Long muzikantId,
+        @RequestParam Long instrumentId,
+        @RequestParam String level
+    ) {
+        Muzikant muzikant = muzikantRepository
+            .findById(muzikantId)
+            .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
 
-  @ExceptionHandler(Exception.class)
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  public String handleError(Model model) {
-    model.addAttribute(
-      "errorMessage",
-      "Er is een fout opgetreden! We konden uw verzoek niet verwerken."
-    );
-    return "error";
-  }
+        Instrument instrument = instrumentRepository
+            .findById(instrumentId)
+            .orElseThrow(() ->
+                new RuntimeException("Instrument niet gevonden")
+            );
+
+        MuzikantInstrument muzikantInstrument = new MuzikantInstrument();
+        muzikantInstrument.setMuzikant(muzikant);
+        muzikantInstrument.setInstrument(instrument);
+        muzikantInstrument.setLevel(level);
+
+        muzikantInstrumentRepository.save(muzikantInstrument);
+
+        return (
+            "redirect:/profile/" + muzikant.getUser().getUsername() + "/edit"
+        );
+    }
+
+    @PostMapping("/profile/remove-instrument")
+    public String removeInstrument(@RequestParam Long muzikantInstrumentId) {
+        Muzikant muzikant = muzikantInstrumentRepository
+            .findById(muzikantInstrumentId)
+            .orElseThrow(() ->
+                new RuntimeException("MuzikantInstrument niet gevonden")
+            )
+            .getMuzikant();
+
+        muzikantInstrumentRepository.deleteById(muzikantInstrumentId);
+        return (
+            "redirect:/profile/" + muzikant.getUser().getUsername() + "/edit"
+        );
+    }
+
+    @PostMapping("/profile/add-genre")
+    public String addGenre(
+        @RequestParam Long muzikantId,
+        @RequestParam Long genreId
+    ) {
+        Muzikant muzikant = muzikantRepository
+            .findById(muzikantId)
+            .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
+
+        Genre genre = genreRepository
+            .findById(genreId)
+            .orElseThrow(() -> new RuntimeException("Genre niet gevonden"));
+
+        muzikant.getGenres().add(genre);
+        muzikantRepository.save(muzikant);
+
+        return (
+            "redirect:/profile/" + muzikant.getUser().getUsername() + "/edit"
+        );
+    }
+
+    @PostMapping("/profile/remove-genre")
+    public String removeGenre(
+        @RequestParam Long muzikantId,
+        @RequestParam Long genreId
+    ) {
+        Muzikant muzikant = muzikantRepository
+            .findById(muzikantId)
+            .orElseThrow(() -> new RuntimeException("Muzikant niet gevonden"));
+
+        muzikant.getGenres().removeIf(genre -> genre.getId().equals(genreId));
+        muzikantRepository.save(muzikant);
+
+        return (
+            "redirect:/profile/" + muzikant.getUser().getUsername() + "/edit"
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleError(Model model) {
+        model.addAttribute(
+            "errorMessage",
+            "Er is een fout opgetreden! We konden uw verzoek niet verwerken."
+        );
+        return "error";
+    }
 }
